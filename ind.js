b990f214112a3b21d65d180e6937a67d5146c1f5
surajpa123 <pathaksuraj136@gmail.com>
2023-10-19 18:34:24 +0530

    added

--- ind.js
+++ ind.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-console.log("Hello")
-console.log("Help m")
-=======
 console.log("hyyyy")
 
 function sum(a,b){
     return a+ b
-}
->>>>>>> 6a7cd1ef168168c32440b91c2904542b2211e00c
+}console.log("Hello")
+console.log("Help m"
